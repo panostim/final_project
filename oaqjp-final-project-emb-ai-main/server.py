a6263d536a3290e19bec1665af9fd6a68ab74ff2
panostim <ptimo8eatos@gmail.com>
2025-09-28 09:43:32 -0400

    Halfway to task 6 v2

--- oaqjp-final-project-emb-ai-main/server.py
+++ oaqjp-final-project-emb-ai-main/server.py
@@ -1,14 +1,27 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, render_template, url_for
 from EmotionDetection.emotion_detection import emotion_detector
 
 # Create an instance of the Flask class, passing in the name of the current module
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def emotionDetector():
-    # Function that handles requests to the root URL
-    # Return a plain text response
-    return emotion_detector('I feel disgusted just hearing about this')
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/emotionDetector/", methods=["GET"])
+def emotionDetectionfunction():
+    if request.method == 'GET':
+        x="Example response"
+        # Redirect to the transactions list page after adding the new transaction
+        return x
+
+@app.route("/emotionDetector/", methods=["POST"])
+def submitText():
+    if request.method == 'POST':
+        #x="Example response"
+        text = request.form.get("textToAnalyze")
+        # Redirect to the transactions list page after adding the new transaction
+        return text
 
 
 if __name__ == "__main__":
