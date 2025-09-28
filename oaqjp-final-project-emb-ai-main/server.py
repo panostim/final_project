5028f70918b976857a5246a105a125570db4c164
panostim <ptimo8eatos@gmail.com>
2025-09-28 13:35:27 -0400

    Completed until 8b - all except 7

--- oaqjp-final-project-emb-ai-main/server.py
+++ oaqjp-final-project-emb-ai-main/server.py
@@ -1,4 +1,8 @@
-from flask import Flask, jsonify, make_response, request, render_template, url_for
+"""
+Flask web application for performing emotion detection on text submitted by the user.
+"""
+
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 # Create an instance of the Flask class, passing in the name of the current module
@@ -6,16 +10,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """
+    URL for handling the html template rendering
+    """
     return render_template("index.html")
-
 
 @app.route("/emotionDetector")
 def emotion_detector_function():
-    # Get the query parameter "textToAnalyze"
+    """
+    URL for handling the user submitted data and the response
+    """
+    # Get the input
     text_to_analyze = request.args.get("textToAnalyze", "")
     # Return it back as plain text
-    text_analysed = emotion_detector(text_to_analyze) 
-    return "For the given statement, the system response is 'anger': " + str(text_analysed["anger"]) + ", 'disgust': " + str(text_analysed["disgust"]) + ", 'fear': " + str(text_analysed["fear"]) + ", 'joy': " + str(text_analysed["joy"]) + " and 'sadness': " + str(text_analysed["sadness"]) + ". The dominant emotion is " + str(text_analysed["dominant_emotion"]) + "."
+    text_analysed = emotion_detector(text_to_analyze)
+    # Return the result
+    return (
+    f"For the given statement, the system response is "
+    f"'anger': {text_analysed['anger']}, "
+    f"'disgust': {text_analysed['disgust']}, "
+    f"'fear': {text_analysed['fear']}, "
+    f"'joy': {text_analysed['joy']} and "
+    f"'sadness': {text_analysed['sadness']}. "
+    f"The dominant emotion is {text_analysed['dominant_emotion']}."
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
